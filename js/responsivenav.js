cb5c53c2291cd7a328835fe76b3eeca1ac0d1c43
vitorregisr <vitorregisrr@gmail.com>
2019-07-22 15:35:12 -0300

    Improve mobile nav javascript

--- js/responsivenav.js
+++ js/responsivenav.js
@@ -1,15 +1,36 @@
 (function () {
     'use strict';
 
-    $('#mobile-menu-toggler').click(function () {
-        $('#mobile-menu').toggleClass('active');
-        $('#header-mobile').toggleClass('active');
-        $('#mobile-search').removeClass('active');
-    });
+    const mobileMenuToggler = $('#mobile-menu-toggler'),
+        mobileSearchToggler = $('#mobile-search-toggler');
 
-    $('#mobile-search-toggler').click(function () {
-        $('#header-mobile').toggleClass('active')
-        $('#mobile-search').toggleClass('active');
-        $('#mobile-menu').removeClass('active');
-    });
+    const mobileSearch = $('#mobile-search'),
+        mobileMenu = $('#mobile-menu'),
+        mobileHeader = $('#mobile-header');
+
+    const toggleHeader = function () {
+        if ( mobileMenu.hasClass('active') || mobileSearch.hasClass('active') ){
+            mobileHeader.addClass('active');
+
+        }else{
+            mobileHeader.removeClass('active');
+        }
+    };
+
+    const toggleMobileMenu = function () {
+        mobileMenu.toggleClass('active');
+        mobileSearch.removeClass('active');
+        mobileMenuToggler.attr('aria-expanded', !mobileMenuToggler.attr('aria-expanded'));
+        toggleHeader();
+    }
+
+    const toggleMobileSearch = function () {
+        mobileSearch.toggleClass('active');
+        mobileMenu.removeClass('active');
+        mobileSearchToggler.attr('aria-expanded', !mobileSearchToggler.attr('aria-expanded'));
+        toggleHeader();
+    };
+
+    mobileMenuToggler.click(toggleMobileMenu);
+    mobileSearchToggler.click(toggleMobileSearch);
 })();
