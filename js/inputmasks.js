b6fc8de10b9122a8942b0042721443f1a317be10
vitorregisr <vitorregisrr@gmail.com>
2019-07-22 21:05:48 -0300

    Improve chart, Improve pages, Add contato page

--- js/inputmasks.js
+++ js/inputmasks.js
@@ -6,4 +6,15 @@
 
      // Stars rating
      $('.stars-rating').barrating({theme: 'fontawesome-stars'});
+
+
+     //Input text masks
+     $('.mask--cpf').mask('000.000.000-00');
+     $('.mask--cep').mask('00000-000');
+
+     //Product color picker scripts
+     $('.radio-color').click( function(){
+        const label = $(this).find('.mask span').attr('data-label');
+        $('#color-name').text(label);
+     });
 })();
